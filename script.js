89a903685c73ba2db6b3bed96fecaac298a2d033
Mathias-Kodehode <mathias.kodehode@gmail.com>
2024-11-09 19:25:17 +0100

    v1

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSection(sectionId) {
+  const sections = document.querySelectorAll(".content-section");
+  sections.forEach((section) => {
+    section.style.display = section.id === sectionId ? "flex" : "none";
+  });
+}
